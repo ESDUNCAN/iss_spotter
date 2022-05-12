76884151f1bd96cb38324724e1145d1ff3a3502d
ESDUNCAN <d.eric@rogers.com>
2022-05-12 23:58:23 +0000

    update

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 
+const { fetchCoordsByIP, fetchMyIP } = require('./iss')
 
-const { fetchMyIP } = require('./iss');
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!", error);
-    return;
-  }
+//   console.log('It worked! Returned IP:', ip);
+// });
 
-  console.log('It worked! Returned IP:', ip);
-});
+fetchCoordsByIP("45.72.204.148", (error, data) => {
+  console.log("Error = ", error)
+  console.log("Data = ", data)
+})
 
--- iss.js
+++ iss.js
@@ -16,4 +16,25 @@ const fetchMyIP = function (callback) {
     callback(null, ip)
   });
 }
-module.exports = { fetchMyIP };
+//module.exports = { fetchMyIP };
+
+const fetchCoordsByIP = function (ip, callback) {
+  let url = "https://api.ipbase.com/v2/info?apikey=SdlZVFBO9Rw9NpiyaAvRMPVFewooAz656tYcwx0W&ip=1.1.1.1"
+  request(url, function (error, response, body) {
+    if (error) {
+      callback(error, null);
+      return
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    const latitude = JSON.parse(body).data.location.latitude;
+    const longitude = JSON.parse(body).data.location.longitude
+    callback(null, { latitude, longitude })
+  });
+}
+
+module.exports = { fetchCoordsByIP, fetchMyIP };
+
